3badf9ae11730a39cc19cc0b4f87c6251c6e2f04
charisteam2 <anhuynhngoc1994@gmail.com>
2023-06-21 20:26:05 +0700

    New

---
+++ next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  staticPageGenerationTimeout: 2000,
+  images: {
+    domains: ['mokubo.website', 'masahiro.me', 'secure.gravatar.com']
+  },
+  async redirects() {
+    return [
+      {
+        source: '/pages',
+        destination: '/pages/1',
+        permanent: true
+      },
+      {
+        source: '/posts',
+        destination: '/pages/1',
+        permanent: true
+      },
+      {
+        source: '/',
+        destination: '/pages/1',
+        permanent: true
+      }
+    ];
+  }
+};
